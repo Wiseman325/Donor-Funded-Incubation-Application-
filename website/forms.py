d5c4e4046d565b0910b708e9ce9264bcfbadbb9e
Msizi Sbonelo Mwelase <110634670+msizi007@users.noreply.github.com>
2024-10-17 00:17:03 +0000

    added user profile

--- website/forms.py
+++ website/forms.py
@@ -19,3 +19,27 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(),
                                                  EqualTo('password', 'password does not match')])
     submit_btn = SubmitField('Register')
+
+class UserForm(FlaskForm):
+    first_name = StringField(label='First Name', validators=[DataRequired()])
+    last_name = StringField(label='Last Name', validators=[DataRequired()])
+    email_address = EmailField(label='Email Address', validators=[DataRequired(), Email()])
+    phone_number = TelField(label='Phone Number', validators=[DataRequired(), Length(min=10, max=10)])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, max=20)])
+    submit_btn = SubmitField('Register')
+
+
+class UpdateProfileForm(FlaskForm):
+    first_name = StringField(label='First Name', validators=[DataRequired()])
+    last_name = StringField(label='Last Name', validators=[DataRequired()])
+    email = EmailField(label='Email Address', validators=[DataRequired(), Email()])
+    phone_number = TelField(label='Phone Number', validators=[DataRequired(), Length(min=10, max=10)])
+
+
+class UpdatePasswordForm(FlaskForm):
+    old_password = PasswordField(label='Old Password', validators=[DataRequired(), Length(min=8, max=20)])
+    new_password = PasswordField(label='New Password', validators=[DataRequired(), Length(min=8, max=20)])
+    confirm_new_password = PasswordField(label='Confirm New Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('new_password', 'password does not match')])
+    submit_btn = SubmitField('Update')
--- website/views.py
+++ website/views.py
@@ -3,6 +3,9 @@ from flask_login import login_required, current_user
 # from .models import SavedFinancialAid, User, FinancialAid
 from . import db
 from datetime import date, datetime
+from .models import User
+from .forms import UpdatePasswordForm, UpdateProfileForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # courses
@@ -26,6 +29,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
+    print(current_user.__dict__)
     return render_template("home.html", user=current_user)
 
 @views.route('/courses')
@@ -33,6 +37,47 @@ def home():
 def courses():
     return render_template("courses.html", courses=courses_list)
 
+
+@views.route('/update_profile/<int:user_id>', methods=['GET', 'POST'])
+def update_profile(user_id):
+    user = User.query.get(user_id)
+    form = UpdateProfileForm(obj=user)
+    form2 = UpdatePasswordForm()
+    if form.validate_on_submit():
+        # Handle form submission and update the driver in the database
+        user.first_name = form.first_name.data
+        user.last_name = form.last_name.data
+        user.email = form.email.data
+        user.phone_number = form.phone_number.data
+
+        db.session.commit()
+
+        flash(f'User #{user._id} updated successfully!', 'success')
+        return redirect(url_for('views.home'))
+    print(form.errors)
+    return render_template("update_profile.html", form=form, form2=form2, user=current_user)
+
+@views.route('/update_password/<int:user_id>', methods=['GET', 'POST'])
+def update_password(user_id):
+    user = User.query.get(user_id)
+    form = UpdatePasswordForm(obj=user)
+    if form.validate_on_submit():
+        if not check_password_hash(user.password, form.old_password.data):
+            flash('Password provided is invalid.', category='error')
+            return redirect(url_for('views.update_profile', user_id=current_user._id))
+        if form.new_password.data != form.confirm_new_password.data:
+            flash('New password does not match.', category='error')
+            return redirect(url_for('views.update_profile', user_id=current_user._id))
+        password=generate_password_hash(form.new_password.data, method='pbkdf2:sha256')
+        # Handle form submission and update the user in the database
+        user.password = password
+
+        db.session.commit()
+
+        flash(f'User #{user._id} updated successfully!', 'success')
+        return redirect(url_for('views.home'))
+    print(form.errors)
+    return render_template("update_profile.html", form=form, user=current_user)
 
 # View financial aids
 # @views.route('/finacial-aid', methods=['GET'])
